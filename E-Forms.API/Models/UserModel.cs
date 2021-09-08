ab6e33d63ab363cdad7879f5c26f21fa96aa4b9f
DESKTOP-VCO1CNH\Maciek <maciej.darlak@gmail.com>
2021-09-08 13:03:19 +0200

    Added Model files

---
+++ E-Forms.API/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace E_Forms.API.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
---
+++ E-Forms.API/Models/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using E_Forms.API.Models;
+
+
+namespace E_Forms.API.Models
+{
+    public class UserRepository : IUserRepository
+    {
+        private List<UserModel> users = new List<UserModel>();
+        private int _nextId = 1;
+
+        public UserRepository()
+        {
+            Add(new UserModel { firstName = "first1", lastName = "last1" });
+            Add(new UserModel { firstName = "first2", lastName = "last2" });
+            Add(new UserModel { firstName = "first3", lastName = "last3" });
+        }
+
+        public UserModel Add(UserModel item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            item.Id = _nextId++;
+            users.Add(item);
+            return item;
+        }
+    }
+}
