1ca6fa0cb9bb94269988d99d6a8df789f76315ca
DESKTOP-VCO1CNH\Maciek <maciej.darlak@gmail.com>
2021-09-08 13:03:55 +0200

    Added UsersController

---
+++ E-Forms.API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using E_Forms.API.Models;
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+
+
+namespace E_Forms.API.Controllers
+{
+    [ApiController]
+    public class UsersController : ControllerBase // ControllerBase : A base class for an MVC controller without view support
+    {
+        private readonly ILogger<UsersController> _logger; // ILogger - error recorder
+        static readonly UserRepository repository = new Models.UserRepository();
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("api/users")]
+        public IEnumerable<UserModel> GetAllUsers()
+        {
+            return repository.GetAll();
+        }
+
+        [HttpPost]
+        [Route("api/user")]
+        [Consumes("application/json")]
+        public UserModel PostUser(UserModel item)
+        {
+            return repository.Add(item);
+        }
+    }
+}
